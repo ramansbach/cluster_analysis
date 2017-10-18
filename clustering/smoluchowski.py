3be957b6e5c57882bf02e2189cf4c8b8a0aae8fc
Rachael Mansbach <ramansbach@gmail.com>
2017-10-18 12:25:33 -0500

    fixed fitting to start at t=0

--- clustering/smoluchowski.py
+++ clustering/smoluchowski.py
@@ -221,7 +221,7 @@ def nonlinearWithErrorsFromFile(fnames,T,dt=1.0,plotstats=None,tstart=0):
         mu2 = [massAvSize(csize) for csize in csizes]
         mu2s[:,f] = mu2
         f+=1
-    (tc,etc,sse,lmbda,elmbda) = nonlinearFit(dt*np.arange(tstart,T+tstart),mu2s,plotstats=plotstats)
+    (tc,etc,sse,lmbda,elmbda) = nonlinearFit(dt*np.arange(T),mu2s,plotstats=plotstats)
     return (tc,etc,sse,lmbda,elmbda)
     
 def linearWithErrors(fnames,T,dt=1.0,fullreturn=False,plotstats=None,tstart=0):
@@ -260,11 +260,11 @@ def linearWithErrors(fnames,T,dt=1.0,fullreturn=False,plotstats=None,tstart=0):
     for fname in fnames:
         csizes = getSizesFromFile([fname],T)
         mu2 = [massAvSize(csize) for csize in csizes]
-        (tc,R2) = linearFit(dt * np.arange(tstart,T+tstart),mu2)
+        (tc,R2) = linearFit(dt * np.arange(T),mu2)
         if plotstats is not None:
             ax = fig.add_subplot(111)
             #pdb.set_trace()
-            runl, = ax.plot(dt * np.arange(tstart,T+tstart),mu2,plotstats[3][i],fillstyle='none')
+            runl, = ax.plot(dt * np.arange(T),mu2,plotstats[3][i],fillstyle='none')
             
             runl.set_label('$R^2$ = {0}'.format(round(R2,2)))
             #ax.legend()
@@ -275,19 +275,19 @@ def linearWithErrors(fnames,T,dt=1.0,fullreturn=False,plotstats=None,tstart=0):
         i+=1
     csizes = getSizesFromFile(fnames,T)
     mu2 = [massAvSize(csize) for csize in csizes]
-    (tc,R2) = linearFit(dt * np.arange(tstart,T+tstart),mu2)
+    (tc,R2) = linearFit(dt * np.arange(T),mu2)
     if fullreturn:
         mu2s[:,len(fnames)] = mu2
     if plotstats is not None:
-        ax.plot(dt * np.arange(tstart,T+tstart,0.1),mu2[0] + (2/tc)*dt*np.arange(0,T,0.1),
+        ax.plot(dt * np.arange(T,0.1),mu2[0] + (2/tc)*dt*np.arange(0,T,0.1),
                  linestyle='--',linewidth=2,color='black')
         #plt.tight_layout()
         Ks = 2/tcs
         sigma = np.std(Ks)
         K = 2/tc
-        ax.fill_between(dt*np.arange(tstart,T+tstart,0.1),mu2[0] \
-                        + (K-sigma)*dt*np.arange(tstart,T+tstart,0.1),
-                         mu2[0]+(K+sigma)*dt*np.arange(tstart,T+tstart,0.1),
+        ax.fill_between(dt*np.arange(T,0.1),mu2[0] \
+                        + (K-sigma)*dt*np.arange(T,0.1),
+                         mu2[0]+(K+sigma)*dt*np.arange(T,0.1),
                         facecolor='black',alpha=0.3)
         plt.xlabel(plotstats[1])
         plt.ylabel(plotstats[2])
@@ -304,11 +304,11 @@ def linearWithErrors(fnames,T,dt=1.0,fullreturn=False,plotstats=None,tstart=0):
             for fname in fnames:
                 mu2curr = mu2s[:,f]
                 plt.figure()
-                plt.plot(dt*np.arange(tstart,T+tstart,0.1),
-                         mu2curr[0]+(2/tcs[f])*dt*np.arange(tstart,T+tstart,0.1),
+                plt.plot(dt*np.arange(0,T,0.1),
+                         mu2curr[0]+(2/tcs[f])*dt*np.arange(0,T,0.1),
                          linestyle='--',linewidth=2,color='black')
-                plt.plot(dt*np.arange(tstart,T+tstart),mu2curr,linewidth=2)
-                plt.plot(dt*np.arange(tstart,T+tstart),mu2curr,'o')
+                plt.plot(dt*np.arange(0,T),mu2curr,linewidth=2)
+                plt.plot(dt*np.arange(0,T),mu2curr,'o')
                 plt.xlabel(plotstats[1])
                 plt.ylabel(plotstats[2])
                 plt.title('run '+str(f))
